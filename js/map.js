813e3ded2d0f3227979af852af1dd7a2e34b4625
Rudy Godoy <rudygodoyg@gmail.com>
2015-06-04 19:36:37 -0500

    Ubicación de casa de investigación. #5

--- js/map.js
+++ js/map.js
@@ -1,18 +1,19 @@
 function showMap() {
     var mapOptions = {
-        center: new google.maps.LatLng(-16.3952829, -71.5367908),
-        zoom: 12,
+        center: new google.maps.LatLng(-16.3893096,-71.5355712),
+        zoom: 17,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     var mapa = new google.maps.Map(document.getElementById("map"), mapOptions);
- 
     var marcador = new google.maps.Marker({
-	    position: new google.maps.LatLng(-16.3897524, -71.53567320000002),
+	    position: new google.maps.LatLng(-16.389150, -71.534852),
 	    map: mapa,
 	  });
-    var contentString = "<div style='width:120px; height:20px; color:#D6A00B; text-align:center; font-weight:bold; font-size:1em;'>Casa de investigación</div>";
+    var contentString = "<div style='width:150px; height:20px; color:#D6A00B; text-align:center; font-weight:bold; font-size:1em;'>Casa de investigación</div>";
     var infowindow = new google.maps.InfoWindow({content:contentString});
-	infowindow.open(mapa, marcador);
-}
+    infowindow.open(mapa, marcador);
+};
 
-showMap();
+google.maps.event.addDomListener(window, 'load', showMap);
+
+
