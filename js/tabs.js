87bc960fc05f0d0e021903979ac3ba9fc4933644
alfa3 <cbegazor@gmail.com>
2016-02-12 13:25:26 -0500

    actualizacion general de la pagina

--- js/tabs.js
+++ js/tabs.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 $('#myTab a').click(function (e) {
     e.preventDefault()
     $(this).tab('show')
 })
-=======
-    $('#myTab a').click(function (e) {
-    e.preventDefault()
-    $(this).tab('show')
-    })
->>>>>>> origin/master
